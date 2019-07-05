f7f86468a8cd48f6378016c02afa9b4e24d3410a
Eric Rovell <ericrovell@gmail.com>
2019-07-05 15:37:00 +0300

    no more rendering bg with canvas, use css instead

--- app/render/render.js
+++ app/render/render.js
@@ -3,10 +3,11 @@ import { renderChaos } from './chaos.js';
 import { getScreenSize } from '../canvas.js';
 
 export function render(context, parameters) {
-  // -> translating to desired origin
-  context.fillStyle = '#081B33';
   let screen = getScreenSize();
-  context.fillRect(-screen.width / 2, -screen.height / 2, screen.width, screen.height);
+
+  // -> used to fill the canvas bg
+  // context.fillStyle = '#081B33';
+  // context.fillRect(-screen.width / 2, -screen.height / 2, screen.width, screen.height);
   
   parameters.vertices = parameters.verticesCoordinates();
   renderFigure(context, parameters);
